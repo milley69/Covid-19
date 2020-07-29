201781750511ffa0b647afbf6e45580554012d50
milley69 <69milley@gmail.com>
2020-07-29 07:19:07 +0500

    adaptive nav

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-// $(document).ready(function() {
-//   $('.header__burger').click(function(event) {
-//     $('nav,.header__burger').toggleClass('active');
-//     $('body').toggleClass('lock');
-//   });
-// });
+$(document).ready(function() {
+  $('.header__burger').click(function(event) {
+    $('nav,.header__burger').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
